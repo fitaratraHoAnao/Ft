331a8d7f809a48d234684845591f54e5f38d96bf
fitaratraHoAnao <brunorakotomalala113@gmail.com>
2025-01-19 12:01:41 +0300

    Update messageHandler.py

--- handler/messageHandler.py
+++ handler/messageHandler.py
@@ -1,92 +1,45 @@
-from database import User
-from util import (
-  text_formatter,
-  getName
-)
-import os
-
-async def get_name(fetchName, uid):
-  try:
-    Users = User()
-    userX = Users.get(uid)
-    if userX:
-      if userX != 'Facebook User':
-        return userX.get('name')
-    else:
-      nameX = getName(uid)
-      if nameX != 'Facebook User':
-        Users.add(uid, nameX)
-        return nameX
-      fetch = await fetchName(uid)
-      tao = fetch.get(uid)
-      name = tao.name
-      Users.add(uid, name)
-      return name
-  except Exception as e:
-    return "Facebook User"
-
-class MessageData:
-  def __init__(self, **data):
-    self.bot = data.get('client')
-    
-    self.cmd = data.get('cmd')
-    self.args = data.get('args')
-    
-    self.mid = data.get('mid')
-    self.author_id = data.get('author_id')
-    self.message = data.get('message')
-    self.message_object = data.get('message_object')
-    self.thread_id = data.get('thread_id')
-    self.thread_type = data.get('thread_type')
-    self.reply = None
-    
-    self.line = "━━━━━━━━━━━━━━━━━━"
-    self.font = text_formatter
-    
-    if self.message_object.replied_to:
-      self.reply = self.message_object.replied_to
-  async def getName(self, uid):
-    name = await get_name(self.bot.fetchUserInfo, self.uid)
-    return name
-  async def sendReply(self, message, auto_font=False, mentions=None):
-    text = self.font(message) if auto_font else message
-    return await self.bot.sendMessage(text, self.thread_id, self.thread_type, reply_to_id=self.mid, mentions=mentions)
-
-async def handleMessage(bot,mid,author_id,message,message_object,thread_id,thread_type,**kwargs):
-  try:
-    _split = message.split(' ',1) if message else ['']
-    cmd, args = _split if len(_split) != 1 else [_split[0],'']
-    cnp = cmd if bot.prefix == "" or not cmd.startswith(bot.prefix) else cmd[1:];cnp = cnp.lower()
-    if cnp in bot.commands:
-      function = bot.commands[cnp]
-      is_need_prefix = function.get('usePrefix', True)
-      is_admin_only = function.get('adminOnly', False)
-      if bot.prefix != '' and is_need_prefix and not cmd.startswith(bot.prefix):
-        return await bot.sendMessage(text_formatter(":mono[This command require to use prefix]"), thread_id, thread_type)
-      elif is_admin_only and str(author_id) not in bot.admin:
-        return await bot.sendMessage(text_formatter(":mono[You dont have permission to use this command.]"), thread_id, thread_type)
-      else:
-        sender = await get_name(bot.fetchUserInfo, author_id)
-        message_data = MessageData(
-          cmd = cnp,
-          args = args,
-          mid = mid,
-          client = bot,
-          author_name = sender,
-          author_id = author_id,
-          message = message,
-          message_object = message_object,
-          thread_id = thread_id,
-          thread_type = thread_type
-        )
-        mtg = f"[blue]ThreadID : [/blue] {thread_id}\n"
-        mtg += f"[blue]Sender   : [/blue] {sender}\n"
-        mtg += f"[blue]Command  : [/blue] [yellow]{cnp}[/yellow]"
-        pnl = bot.logInfo(mtg, title=author_id)
-        return await function['def'](bot, message_data)
-  except bot.FBchatFacebookError as err:
-    bot.error(f"{err}", 'FBchatFacebookError')
-  except bot.FBchatException as err:
-    bot.error(f"{err}", 'FBchatException')
-  except Exception as err:
-    bot.error(f"{err}", 'Exception')
+async def handleMessage(bot, mid, author_id, message, message_object, thread_id, thread_type, **kwargs):
+    try:
+        _split = message.split(' ', 1) if message else ['']
+        cmd, args = _split if len(_split) != 1 else [_split[0], '']
+        
+        # Vérifier si 'prefix' existe avant de l'utiliser
+        if hasattr(bot, 'prefix') and bot.prefix != "" and cmd.startswith(bot.prefix):
+            cnp = cmd[1:].lower()
+        else:
+            cnp = cmd.lower()
+        
+        if cnp in bot.commands:
+            function = bot.commands[cnp]
+            is_need_prefix = function.get('usePrefix', True)
+            is_admin_only = function.get('adminOnly', False)
+            
+            if bot.prefix != '' and is_need_prefix and not cmd.startswith(bot.prefix):
+                return await bot.sendMessage(text_formatter(":mono[This command requires the use of a prefix]"), thread_id, thread_type)
+            elif is_admin_only and str(author_id) not in bot.admin:
+                return await bot.sendMessage(text_formatter(":mono[You don't have permission to use this command.]"), thread_id, thread_type)
+            else:
+                sender = await get_name(bot.fetchUserInfo, author_id)
+                message_data = MessageData(
+                    cmd=cnp,
+                    args=args,
+                    mid=mid,
+                    client=bot,
+                    author_name=sender,
+                    author_id=author_id,
+                    message=message,
+                    message_object=message_object,
+                    thread_id=thread_id,
+                    thread_type=thread_type
+                )
+                mtg = f"[blue]ThreadID : [/blue] {thread_id}\n"
+                mtg += f"[blue]Sender   : [/blue] {sender}\n"
+                mtg += f"[blue]Command  : [/blue] [yellow]{cnp}[/yellow]"
+                pnl = bot.logInfo(mtg, title=author_id)
+                return await function['def'](bot, message_data)
+    except bot.FBchatFacebookError as err:
+        bot.error(f"{err}", 'FBchatFacebookError')
+    except bot.FBchatException as err:
+        bot.error(f"{err}", 'FBchatException')
+    except Exception as err:
+        bot.error(f"{err}", 'Exception')
